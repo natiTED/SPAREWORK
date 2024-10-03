45e62668cdcaa72ed2be0198c50c184fc7e6377f
natiTED <123365384+natiTED@users.noreply.github.com>
2024-10-03 18:18:32 +0300

    Update server.js

--- spareworkAPI/server.js
+++ spareworkAPI/server.js
@@ -59,7 +59,7 @@ app.use((err, req, res, next) => {
   return res.status(errorStatus).send(errorMessage);
 });
 
-app.listen(port, () => {
+app.listen(8800, () => {
   connect();
   console.log("Server is running on port 8800");
 });
