13bf66ed2d1668cf7de5b6f1ab97fc20d1e0c5a4
natiTED <123365384+natiTED@users.noreply.github.com>
2024-10-03 16:13:48 +0300

    Update server.js

--- spareworkAPI/server.js
+++ spareworkAPI/server.js
@@ -25,7 +25,7 @@ const connect = async () => {
   }
 };
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://glittering-eclair-4828d3.netlify.app", credentials: true }));
 
 app.use(express.json());
 app.use(cookieParser());
